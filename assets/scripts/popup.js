260268a953938a925e1676cf91ddcae701ac8c61
TheKevSter35 <43183768+TheKevSter35@users.noreply.github.com>
2021-11-07 18:52:36 +0100

    add popup function

---
+++ assets/scripts/popup.js
@@ -0,0 +1,18 @@
+  function setpopup(pic) {
+      let app = document.querySelector('main');
+      let setModal = `
+              <div class = "modal">
+               <img src= "${pic}"  >
+               <span> x </span>
+              </div>
+            `;
+      app.insertAdjacentHTML('beforeend', setModal);
+      let close = document.querySelector('span');
+      close.addEventListener("click", closeModal);
+
+      function closeModal(pic) {
+        let modal = document.getElementsByClassName('modal')[0]
+        modal.remove()
+
+      }
+    }
